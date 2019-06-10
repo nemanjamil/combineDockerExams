471ea6495d36590777e506fe17a65f9cd0fb326c
nemanjamil <nemanjamil@gmail.com>
2019-06-10 19:31:07 +0200

    Initial Commit

---
+++ BE/app/Sqms_exam_version.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Sqms_exam_version extends Model
+{
+    //
+
+    //protected $table = 'sqms_exam_version';
+    protected $table = 'v_sqms_exam_version_sample_set';
+}
